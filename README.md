173774b91ca39e6bf03a6774a933e94d63c39786
a5mith <aaronsmith91@sky.com>
2014-05-31 16:16:55 +0100

    0.0.1
    First Sweep

---
+++ README.md
@@ -0,0 +1,13 @@
+# NodeBB Vine Plugin
+
+This NodeBB plugin allows users to embed Vine widgets inline in their posts.
+
+## Installation
+
+    npm install nodebb-plugin-vine
+
+## Usage
+
+    Paste the URL into a post.
+    ???
+    Profit!
---
+++ library.js
@@ -0,0 +1,14 @@
+(function(module) {
+    "use strict";
+
+    var Vine = {},
+        embed = '<iframe class="vine-embed" src="https://vine.co/$1/embed/postcard" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>'
+
+
+    Vine.parse = function(postContent, callback) {
+        postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:vine\.co)\/?(.+)">.+<\/a>/g, embed);
+        callback(null, postContent);
+    };
+
+    module.exports = Vine;
+}(module));
