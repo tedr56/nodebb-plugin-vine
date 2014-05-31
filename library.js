71c22acb6bd5a9949addcdc62ebebc26ee6c9967
a5mith <aaronsmith91@sky.com>
2014-05-31 16:22:05 +0100

    Le Sigh

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var Vine = {},
-        embed = '<iframe class="vine-embed" src="https://vine.co/$1/embed/postcard" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>'
+        embed = '<iframe class="vine-embed" src="https://vine.co/$1/postcard" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>'
 
 
     Vine.parse = function(postContent, callback) {
