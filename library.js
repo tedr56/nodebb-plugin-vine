a714b7ba25e7e731ee47ad35efaf4c2a099cf244
a5mith <aaronsmith91@sky.com>
2014-11-18 16:55:22 +0000

    0.6.0

--- library.js
+++ library.js
@@ -2,11 +2,18 @@
     "use strict";
 
     var Vine = {},
-        embed = '<iframe class="vine-embed" src="https://vine.co/v/$1/embed/postcard?related=0" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>'
+        embed = '<iframe class="vine-embed" src="https://vine.co/v/$1/embed/postcard?related=0" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>';
+    var link = /<a href="(?:https?:\/\/)?(?:vine\.co)\/v\/?(.+)">.+<\/a>/g;
 
-    Vine.parse = function(postContent, callback) {
-        postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:vine\.co)\/v\/?(.+)">.+<\/a>/g, embed);
-        callback(null, postContent);
+    Vine.parse = function(data, callback) {
+        if (!data || !data.postData || !data.postData.content) {
+            return callback(null, data);
+        }
+        if (data.postData.content.match(link)) {
+            data.postData.content = data.postData.content.replace(link, embed);
+        }
+        callback(null, data);
+
     };
 
     module.exports = Vine;
