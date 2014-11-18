a9b32aa604330d1371b20998f2fd8c6d8da01a03
TedVJ <tedr56@gmail.com>
2014-11-18 17:27:52 +0100

    NodeBB 0.6.0 Compatibility - Update to 0.0.4

--- library.js
+++ library.js
@@ -4,9 +4,15 @@
     var Vine = {},
         embed = '<iframe class="vine-embed" src="https://vine.co/v/$1/embed/postcard?related=0" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>'
 
-    Vine.parse = function(postContent, callback) {
-        postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:vine\.co)\/v\/?(.+)">.+<\/a>/g, embed);
-        callback(null, postContent);
+    var embedUrl = /<a href="(?:https?:\/\/)?(?:vine\.co)\/v\/?(.+)">.+<\/a>/g;
+    Vine.parse = function(data, callback) {
+	    if (!data || !data.postData || !data.postData.content) {
+          return callback(null, data);
+        }
+        if (data.postData.content.match(embedUrl)) {
+          data.postData.content = data.postData.content.replace(embedUrl, embed);
+        }
+        callback(null, data);
     };
 
     module.exports = Vine;
